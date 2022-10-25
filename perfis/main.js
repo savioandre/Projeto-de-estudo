6613fbb1df5ef040148fcb9327be5d5eb4c0419b
Sávio André <savioa834@gmail.com>
2022-10-25 11:01:11 -0300

    Final Version

--- perfis/main.js
+++ perfis/main.js
@@ -1,6 +1,7 @@
 const len = document.querySelectorAll('.rs').length;
 const index = document.querySelector('section').getAttribute('data-index');
 var pro = JSON.parse(localStorage.getItem(`profile${parseInt(index)}`)) || [];
+const namePage = `profile${parseInt(index)}`
 
 let teste = 0;
 while (teste < len) {
@@ -10,25 +11,29 @@ while (teste < len) {
     })
 };
 
-let res = document.querySelector('#btm');
-res.addEventListener('click', () => {
-    let id = 0;
-    while (id < len) {
-        let a = document.getElementById(id).value;
-        pro.push({
-            ask: a
-        });
-        localStorage.clear();
-        localStorage.setItem(`profile${parseInt(index)}`, JSON.stringify(pro));
-        id += 1;
-    }
-    alert('Respostas Salvas!');
-});
+document.querySelectorAll('textarea').forEach((i) => {
+    i.addEventListener('keyup', (e) => {
+        let k = e.key
+        setTimeout(() => {
+            let id = 0;
+            pro = [];
+
+            while (id < len) {
+                let a = document.getElementById(id).value;
+                pro.push({
+                    ask: a
+                });
+                id += 1;
+            };
+            localStorage.setItem(namePage, JSON.stringify(pro));
+        }, 1000);
+    })
+})
 
 if (localStorage.length > 0) {
     let i = 0;
     while (i <= len) {
-        let gf = JSON.parse(localStorage.getItem(`profile${parseInt(index)}`))[i].ask;
+        let gf = JSON.parse(localStorage.getItem(namePage))[i].ask;
         document.getElementById(i).innerHTML = gf;
         i += 1;
     }
